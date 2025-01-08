8303f6f5955e4d76ba38079f7707517c8caf3fc1
Lauren Farrell <150389282+BUFFALOLO@users.noreply.github.com>
2025-01-08 11:01:41 -0500

    Module5Lesson2.sql

--- Module5Lesson2.sql
+++ Module5Lesson2.sql
@@ -15,27 +15,26 @@ CREATE TABLE WorkoutSessions (
 
 INSERT INTO Members (id, name, age)
 VALUES 
-(1, 'John Doe', 30),
-(2, 'Jane Doe', 28),
-(3, 'Michael Johnson', 35),
-(4, 'Emily Brown', 25),
-(5, 'David Davis', 40);
+(1, 'Lauren Smith', 31),
+(2, 'Matt Smith', 34),
+(3, 'Jane Doe', 21),
+(4, 'John Doe', 25),
 
 INSERT INTO WorkoutSessions (session_id, member_id, session_date, session_time, activity)
 VALUES 
-(1, 1, '2024-09-01', '08:00 AM', 'Cardio'),
-(2, 2, '2024-09-01', '06:00 AM', 'Yoga'),
-(3, 3, '2024-09-02', '07:00 AM', 'Strength Training'),
-(4, 4, '2024-09-03', '05:30 PM', 'Cycling'),
-(5, 2, '2024-09-04', '06:00 AM', 'Cardio');
+(1, 1, '2025-01-05', '07:00 AM', 'Cardio'),
+(2, 2, '2025-01-05', '08:00 AM', 'Yoga'),
+(3, 3, '2025-01-05', '09:00 AM', 'Strength Training'),
+(4, 4, '2025-01-05', '10:30 AM', 'Cycling'),
+(5, 2, '2025-01-05', '11:00 AM', 'Cardio');
 
 UPDATE WorkoutSessions
-SET session_time = '06:00 PM'
-WHERE member_id = 2 AND session_date = '2024-09-01';
+SET session_time = '09:00 AM'
+WHERE member_id = 2 AND session_date = '2025-01-05';
 
 
-SELECT id FROM Members WHERE name = 'John Smith';
+SELECT id FROM Members WHERE name = 'Matt Smith';
 
-DELETE FROM WorkoutSessions WHERE member_id = 6;
+DELETE FROM WorkoutSessions WHERE member_id = 2;
 
-DELETE FROM Members WHERE id = 6;
+DELETE FROM Members WHERE id = 2;
